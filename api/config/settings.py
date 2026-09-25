bbffacde52ae3ac655df5a9e41ba60531c82ba71
Bruno Dias <brunoipb1@gmail.com>
2026-09-25 14:44:45 -0300

    chore: add credenciais db e redis

--- api/config/settings.py
+++ api/config/settings.py
@@ -73,9 +73,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "cinepass", "USER": "cinepass", "PASSWORD": "cinepass",
+        "HOST": "db", "PORT": "5432",
+    }
+}
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/1",
     }
 }
 
